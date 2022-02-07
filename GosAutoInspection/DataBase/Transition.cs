2ba85824abc4bd5308ddae4dfc2c257c950dcf18
315 <315@kp15.lan>
2022-02-07 12:50:26 +0300

    Добавьте файлы проекта.

---
+++ GosAutoInspection/DataBase/Transition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GosAutoInspection.DataBase
+{
+    class Transition
+    {
+        public static Frame MainFrame { get; set; }
+
+        private static GosAutoInspectionEntities context;
+        public static GosAutoInspectionEntities Context
+        {
+            get
+            {
+                if (context == null)
+                    context = new GosAutoInspectionEntities();
+
+                return context;
+            }
+        }
+    }
+}
---
+++ GosAutoInspection/Pages/DriverList.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GosAutoInspection.DataBase;
+
+namespace GosAutoInspection.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для DriverList.xaml
+    /// </summary>
+    public partial class DriverList : Page
+    {
+        public DriverList()
+        {
+            InitializeComponent();
+            DriversListView.ItemsSource = Transition.Context.Drivers.ToList();
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DltBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
